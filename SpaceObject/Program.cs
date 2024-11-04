7aedceec0b423f6f0c8524bf73da56b6aa748d8d
Kostiantyn Polishko <167745085+KostiantynPolishko2@users.noreply.github.com>
2024-11-04 22:26:13 +0200

    release v1.1.0 SpaceObjects (#5)
    * release v1.0.0 SpaceObjectServer (#1)
    
    * Create 20241031_AzureDbMigration.sln
    
    * 2 connected asp.net project SpaceObject
    
    * 3 created db space_objects
    
    classes AsteroidItems HasOne with One AsteroidProperty
    
    * 4 Done HttpGet Name = "GetAsteroidsItem", Name = "GetAsteroidsProperty"
    
    * 5 Done HttpGet Name = "GetAsteroidsInfoByType"
    
    * 6 Done HttpDelete and HttpPost
    
    * 7 Done HttpPatch
    
    * 8 recconected to Azure SQL server DB space_objects
    
    * 9 Created class MyJPIF for JSON HttpPatch
    
    * release v1.0.0 Authorization (#3) (#4)
    
    * 1 created default project asp.net
    
    * 2 created infrastructure for server Authorization
    
    * 3 executed migration to sql db
    
    * 1 Managed Authoriation of SpaceObjectController
    
    * 2 fixed name of connection string of sql db Authorization and SpaceObjects

---
+++ SpaceObject/Program.cs
@@ -0,0 +1,92 @@
+using SpaceObject.EF;
+using Microsoft.EntityFrameworkCore;
+using SpaceObject;
+using Microsoft.OpenApi.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+//Server SpaceObjects
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddDbContext<SpaceObjectContext>(configure => configure.UseSqlServer(builder.Configuration.GetConnectionString("LocalSqlDb_SpaceObject")));
+//builder.Services.AddControllers().AddNewtonsoftJson();
+builder.Services.AddControllers(options =>
+{
+    options.InputFormatters.Insert(0, MyJPIF.GetJsonPatchInputFormatter());
+});
+
+// Adding Authentication
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.SaveToken = true;
+    options.RequireHttpsMetadata = false;
+    options.TokenValidationParameters = new TokenValidationParameters()
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidIssuer = builder.Configuration["JWT:ValidIssuer"],
+        ValidAudience = builder.Configuration["JWT:ValidAudience"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JWT:Secret"]!))
+    };
+});
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "SpaceObjects.Api", Version = "v1" });
+
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = @"JWT Authorization header using the Bearer scheme. \r\n\r\n
+                      Enter 'Bearer' [space] and then your token in the text input below.
+                      \r\n\r\nExample: 'Bearer 12345abcdef'",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer"
+    });
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                },
+                Scheme = "oauth3",
+                Name = "Bearer",
+                In = ParameterLocation.Header
+            },
+            new List<string>()
+        }
+    });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
