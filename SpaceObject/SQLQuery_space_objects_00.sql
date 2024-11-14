28de114d2de7da01e9543c02d13e3cf1510cebca
Kostiantyn Polishko <polxs_wp31@student.itstep.org>
2024-11-14 22:07:35 +0200

    switched connedction string to LocalSqlDb

--- SpaceObject/SQLQuery_space_objects_00.sql
+++ SpaceObject/SQLQuery_space_objects_00.sql
@@ -12,10 +12,28 @@ VALUES
 (512, 2.11, 1, 2),
 (234, 2.82, 1, 3);
 
+INSERT INTO [dbo].[asteroid_images] (name, path, FK_IdAsteroidItem)
+VALUES
+('cerera', 'https://docfiles.blob.core.windows.net/files/asteroid/cerera.png', 1),
+('pallada', 'https://docfiles.blob.core.windows.net/files/asteroid/pallada.png', 2),
+('yunona', 'https://docfiles.blob.core.windows.net/files/asteroid/yunona.png', 3),
+('astreya', 'https://docfiles.blob.core.windows.net/files/asteroid/astreya.png', 5);
+
 SELECT * FROM [dbo].[asteroid_items];
 SELECT * FROM [dbo].[asteroid_properties];
+SELECT * FROM [dbo].[asteroid_images];
 
-SELECT [ai].[name], 'class ' + [ai].[type] AS [category], [ap].[size], [ap].[weight], [ap].[speed] FROM [dbo].[asteroid_items] AS [ai]
+SELECT [ai].[name], 'class ' + [ai].[type] AS [category], [ap].[size], [ap].[weight], [ap].[speed], [am].[path] FROM [dbo].[asteroid_items] AS [ai]
 INNER JOIN [dbo].[asteroid_properties] AS [ap]
 ON [ai].[id] = [ap].[FK_IdAsteroidItem]
+INNER JOIN [dbo].[asteroid_images] AS [am]
+ON [ai].[id] = [am].[FK_IdAsteroidItem]
 WHERE [ai].[type] = 'c'
+
+SELECT * FROM [dbo].[__EFMigrationsHistory];
+
+INSERT INTO [dbo].[__EFMigrationsHistory] (MigrationId, ProductVersion)
+VALUES
+('20241103094705_SpaceObjects_00', '8.0.10'),
+('20241103132921_SpaceObjects_01', '8.0.10'),
+('20241103140909_SpaceObjects_02', '8.0.10');
